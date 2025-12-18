6b3c02c5a461ae9ea0996a6c6c8f0b0d149fad3e
breaddaerb <gongzhlin@gmail.com>
2025-12-18 22:31:08 +0800

    set colorscheme to tokyo-night.
    note that mac default terminal doesn't support tokyo-night. use ghostty/iterm/etc

--- init.lua
+++ init.lua
@@ -94,7 +94,6 @@ vim.opt.clipboard = 'unnamedplus' --using system clipboard
 vim.opt.hlsearch = true
 vim.o.ttimeout = true
 vim.o.timeoutlen = 100
-vim.cmd.colorscheme 'quiet'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
@@ -901,21 +900,21 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    --    'folke/tokyonight.nvim',
-    --    priority = 1000, -- Make sure to load this before all the other start plugins.
-    --   config = function()
-    --      ---@diagnostic disable-next-line: missing-fields
-    --      require('tokyonight').setup {
-    --        styles = {
-    --          comments = { italic = false }, -- Disable italics in comments
-    --        },
-    --      }
+       'folke/tokyonight.nvim',
+       priority = 1000, -- Make sure to load this before all the other start plugins.
+      config = function()
+         ---@diagnostic disable-next-line: missing-fields
+         require('tokyonight').setup {
+           styles = {
+             comments = { italic = false }, -- Disable italics in comments
+           },
+         }
 
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    --      vim.cmd.colorscheme 'tokyonight-night'
-    --    end,
+         vim.cmd.colorscheme 'tokyonight-night'
+       end,
   },
 
   -- Highlight todo, notes, etc in comments
